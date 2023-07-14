2c0b4960665d52b95d24ffe5b2d9c30202e399c0
priya_n2022 <glenraise@gmail.com>
2023-07-14 11:11:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Month from './components/Months/Month'
+import Home from './components/Home/Home'
+
+
+
+const App = () => {
+  return (
+    <> 
+
+    
+    <div>
+    <Home />    
+    <Month />
+    </div>
+    </>
+  )
+}
+
+export default App
